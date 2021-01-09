62b5c03a4f256fb648134c078fb461dff212ff80
Bullet47 <1249272751@qq.com>
2021-01-09 22:03:44 +0800

    add  最多的单词 file

---
+++ 最多的单词.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<string.h>
+struct M{
+	
+	char str[50];
+	int nm;
+};
+int main(void)
+{   
+    int i=0;
+	struct M s[50];
+	char temp,host[100]={0};
+	while((temp=getchar())!=EOF)
+	{
+		host[i]=temp;
+		if(host[i]>='A'&&host[i]<='Z')
+		 {
+		 	host[i]=host[i]+'a'-'A';
+		 }
+		 i++;
+	}
+	int j,k,l,m,n;
+	m=n=0;
+	for(i=0;i<strlen(host);i++)
+	{
+		s[m].str[n]=host[i];
+		n++;
+		if(host[i+1]>'z'||host[i+1]<'a')
+		{
+		  m++;
+		  n=0;
+		}
+	}
+	for(i=0;i<m;i++)
+	{
+		for(k=0;k<m;k++)
+		{
+			if(strcmp(s[i].str,s[k].str)==0)
+			{
+				s[i].nm++;
+			}
+		}
+	}
+	int max=-1;
+	int nmax=0;
+	for(i=0;i<m;i++)
+	{
+		if(s[i].nm>max||(s[i].nm==s[nmax].nm&&strcmp(s[i].str,s[nmax].str)>0))
+		{
+			max=s[i].nm;
+			nmax=i;
+		}
+	}
+	printf("%s %d",s[nmax].str,s[nmax].nm);
+	return 0;
+}
