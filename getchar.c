b4f0782c7d85ca21b65d413137b51f5476a2a0b5
Bullet47 <1249272751@qq.com>
2021-01-09 22:58:53 +0800

    add getchar file

---
+++ getchar.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+//utf8_to_gb2312
+	string utf8_to_gb2312(const char* utf8)
+	{
+		int len = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, NULL, 0);
+		wchar_t* wstr = new wchar_t[len + 1];
+		memset(wstr, 0, len + 1);
+		MultiByteToWideChar(CP_UTF8, 0, utf8, -1, wstr, len);
+		len = WideCharToMultiByte(CP_ACP, 0, wstr, -1, NULL, 0, NULL, NULL);
+		char* str = new char[len + 1];
+		memset(str, 0, len + 1);
+		WideCharToMultiByte(CP_ACP, 0, wstr, -1, str, len, NULL, NULL);
+		string strTemp = str;
+		if (wstr) 
+			delete[] wstr;
+		if (str)
+			delete[] str;
+ 
+		return strTemp;
+	}
+ 
+	//gb2312_to_utf8
+	string gb2312_to_utf8(const char* gb2312)
+	{
+		int len = MultiByteToWideChar(CP_ACP, 0, gb2312, -1, NULL, 0);
+		wchar_t* wstr = new wchar_t[len + 1];
+		memset(wstr, 0, len + 1);
+		MultiByteToWideChar(CP_ACP, 0, gb2312, -1, wstr, len);
+		len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+		char* str = new char[len + 1];
+		memset(str, 0, len + 1);
+		WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, len, NULL, NULL);
+		string strTemp = str;
+		if (wstr)
+			delete[] wstr;
+		if (str)
+			delete[] str;
+ 
+		return strTemp;
+	}
