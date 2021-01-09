c75dc33602ba3fb6adbe11d89793aa56da5e27a0
Bullet47 <1249272751@qq.com>
2021-01-09 23:00:42 +0800

    add strlen file

---
+++ strlen.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h> 
+int main(void)
+{ int i   ;
+ char str1[3][4];
+ for(i=0;i<=2;i++)
+ {sscanf("%s",str1[i][]) ;
+ }
+ printf("%s  %d",str1,strlen(str1));
+ return 0;
+ } 
