7248167b1001a0b2746cbbb1071efd05aa17f275
Bullet47 <1249272751@qq.com>
2021-01-10 16:11:14 +0800

    add 第六周第一题 file

---
+++ 第五周第三题.c
@@ -0,0 +1,150 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int main(void)
+{
+	int n,i,k,m,line,q,r,temp;
+	float f;
+	char  x[100];
+	scanf("%d",&n);
+	for(i=0,r=0;i<n;i++)
+	{   
+	    memset(x,0,sizeof(x));
+		scanf("%d",&temp);
+        sprintf(x,"%d",temp);//itoaÀàËÆ
+		printf("%s:\n",x) ;	
+     for(line=1;line<=7;line++)
+		{
+			for(k=0;k<strlen(x);k++)
+			{   
+				if(x[k]=='0')
+				{
+					if(line==1||line==7)
+					{
+					 	printf("*****");
+					}
+					if(line>=2&&line<=6)
+					{
+						printf("*   *");
+					}	
+				}
+				if(x[k]=='1')
+				{
+                  printf("    *");	
+				}
+				if(x[k]=='2')
+				{
+				  	if(line==1||line==4||line==7)
+					{
+						printf("*****");
+					}
+					if(line==2||line==3)
+					{
+						printf("    *");
+					}
+					if(line==5||line==6)
+					printf("*    ");
+				}
+				if(x[k]=='3')
+				{
+				 	if(line==1||line==4||line==7)
+					{
+						printf("*****");
+					}
+                    if(line==2||line==3||line==5||line==6)
+                    {
+                     printf("    *");	
+					}
+				}
+				if(x[k]=='4')
+				{
+					if(line>=1&&line<=3)
+					{
+						printf("*   *");
+					}
+					if(line==4)
+					{
+						printf("*****");
+					}
+					if(line>=5&&line<=7)
+					{
+						printf("    *");
+					}
+
+				}
+				if(x[k]=='5')
+				{
+					if(line==1||line==4||line==7)
+					{
+						printf("*****");
+					}
+					if(line==5||line==6)
+					{
+						printf("    *");
+					}
+					if(line==2||line==3)
+					{
+						printf("*    ");
+					}
+				}
+				if(x[k]=='6')
+				{
+                   if(line==1||line==4||line==7)
+					{
+						printf("*****");
+					}
+				   if(line==2||line==3)
+					{
+						printf("*");
+					}
+					if(line==5||line==6)
+					{
+						printf("*   *");
+					}
+				}
+				if(x[k]=='7')
+				{
+					if(line==1)
+					{
+						printf("*****");
+					}
+                    else
+                    {
+                    	printf("    *");
+					}
+				}
+				if(x[k]=='8')
+				{
+				if(line==1||line==4||line==7)
+					{
+						printf("*****");
+					}
+                  else
+                  {
+                  	    printf("*   *");
+				  }
+				}
+				if(x[k]=='9')
+				{
+				   if(line==1||line==4||line==7)
+					{
+						printf("*****");
+					}
+					if(line==2||line==3)
+					{
+					    printf("*   *")	;
+					}
+                    if(line==5||line==6)
+                    {
+                    	printf("    *");
+					}
+				}
+				printf("  ");		
+			}
+			printf(" ");
+			printf("\n");	
+		}
+	}
+	
+	return 0;	
+}
